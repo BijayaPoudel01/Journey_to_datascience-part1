41bef8472885a96dcdd244eb20bb56008bb7a34d
BijayaPoudel01 <bijayapoudel32@gmail.com>
2025-03-21 16:42:46 +0545

    Learned basics on my first day of online python course.

---
+++ Day1_and_Day2class.py
@@ -0,0 +1,87 @@
+# name="ram"
+# print(name)
+#
+# a=67
+# print(type(a))
+#
+# #day2
+# a="my name is bijaya"
+# print(a[8:17:1])
+# name="kathmandu"
+# print(name[0:9:1])
+# print(name[1:8:1])
+# print(name[2:7:1])
+# print(name[3:6:1])
+# print(name[4:5:1])
+# b=(a.upper())
+# print(b)
+# c=(b.lower())
+# print(c)
+# b=a.swapcase()
+# print(b)
+# d=a.startswith("m")
+# print(d)
+# d= a.find("a")
+# print(d)
+#
+
+print("hello world")
+'''hi 
+i 
+am ''' #this is multi line comment
+
+d=80
+print(type(d))
+
+name= "welcome to \t sipalaya"
+print(name)
+name= "welcome to \n sipalaya"
+print(name)
+
+print(name[8])
+print(name[-2]) #indexing
+
+a="hello i am from ilam"
+print(a[0:20:1]) #slicing
+print(a[16:20:1])
+print(a[19:15:-1])
+print(a[19::-1])
+
+a="Bijaya"
+print(a[0:6:1])
+print(a[1:5:1])
+print(a[2:4:1])
+print(a[3:4:1])
+
+#now reverse
+print(a[5::-1])
+print(a[4:0:-1])
+print(a[3:1:-1])
+print(a[2:1:-1])
+
+#string inbuilt methods
+a="ram"
+print(a)
+b=(a.upper())
+print(b)
+# c=(b.lower())
+# print(c)
+
+#swapcase
+b=a.swapcase()
+print(b)
+
+ram="bijaya"
+print(ram.replace("j", "p", 2 ))
+
+print(ram.capitalize())
+print(ram.startswith("b"))
+print(ram.endswith("e"))
+print(ram.count("a"))
+print(ram.find("j"))
+# print(ram.index("z"))
+
+s= "ram, hari, shyam"
+print(s.split(","))
+
+
