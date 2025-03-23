c8cb19a872d6b43c63ce6cfcea1a6905f44ee683
BijayaPoudel01 <bijayapoudel32@gmail.com>
2025-03-23 17:30:02 +0545

    python learning

---
+++ Day3class.py
@@ -0,0 +1,18 @@
+my_list=[10, 20, 30, 40, 50]
+third_element =(my_list[2])
+print(third_element)
+
+my_list=[10,20,30,40,50]
+new_list=(my_list[0:3])
+print(new_list)
+
+str = "apple, banana, cherry"
+a= str.split(",")
+print(a)
+
+my_list = [1, 2, 3]
+new_list = my_list * 3 #replicating
+print(new_list)
+
+numbers=[1,2,3,4,5,6]
+print(numbers[::2])
